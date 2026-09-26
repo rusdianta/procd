0a6f7fbcbac1bc211ea994aa46be5464c2da6e54
rusdianta <rusdianta@gmail.com>
2026-09-26 22:50:16 +0800

    jail: mount more stuff read-only Mount /etc/resolv.conf, /etc/passwd, /etc/group and /etc/nsswitch.conf read-only in ujail slim-containers.
    Signed-off-by: Daniel Golle <daniel@makrotopia.org>

--- jail/jail.c
+++ jail/jail.c
@@ -488,12 +488,12 @@ int main(int argc, char **argv)
 		add_mount("/dev/zero", 0, -1);
 
 		if (opts.user || opts.group) {
-			add_mount("/etc/passwd", 0, -1);
-			add_mount("/etc/group", 0, -1);
+			add_mount("/etc/passwd", 1, -1);
+			add_mount("/etc/group", 1, -1);
 		}
 
 		if (!(opts.namespace & CLONE_NEWNET)) {
-            add_mount("/etc/resolv.conf", 0, -1);
+            add_mount("/etc/resolv.conf", 1, -1);
         }
 
 		int flags = CLONE_NEWPID | CLONE_NEWNS | CLONE_NEWIPC | SIGCHLD;
